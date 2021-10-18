8bdb081276d723926088f6d1a3e3958c377d43fd
olinil <e-olinil@asvg.no>
2021-10-18 12:55:43 +0200

    cwe

---
+++ .vscode/server.py
@@ -0,0 +1,7 @@
+PORT = 80
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
